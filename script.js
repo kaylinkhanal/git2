80e6d7826c7ca73e1764c8f7593348369746d686
bibek-404 <bhattabibek2004@gmail.com>
2025-12-01 07:57:12 +0545

    something changed

--- script.js
+++
@@ -1,7 +0,0 @@
-//! Replace part of a string
-let word = 'typescript';
-
-//! Q1: Replace 'type' with 'java'
-//? Expected Output: "javascript"
-let newWord = word.replace('type', 'java');
-console.log(newWord); // Output: javascript
