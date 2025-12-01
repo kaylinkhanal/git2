1f8a731e703e1d9443698174758257f771863859
aswinbaral2 <aswinbaral2@gmail.com>
2025-12-01 08:33:19 +0545

    added constants and function math

--- newproject.js
+++ newproject.js
@@ -1,1 +1,15 @@
 let a = 10
+let b = 20
+
+let math = a + b    
+const scores = {
+    math: 120,
+    english: 100,
+    science: 90,
+}   
+let sum =0
+for (let item in scores ) {
+sum = sum + scores[item]
+}
+
+console.log(`Total percentage is: ${sum/450 * 100}%`);
